c73b0913d53377024bd5c1c61d24980a6eed980b
Hadiuzaman <hadi.mmpi@gmail.com>
2020-06-04 20:55:16 +0600

    commit  git commit -m  commit

---
+++ SceneBulder.java
@@ -0,0 +1,31 @@
+
+package scenebulder;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class SceneBulder extends Application {
+
+    
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent pane = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        
+        
+        Scene secne = new Scene(pane);
+        
+        primaryStage.setScene(secne);
+        primaryStage.show();
+    }
+    
+}
